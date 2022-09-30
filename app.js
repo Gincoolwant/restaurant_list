969c285471fb24ba063787ea3c2d8851612bc0b6
Gincoolwant <soulbox790326@gmail.com>
2022-09-30 14:31:08 +0800

    [init]Project initital

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+const exphbs = require('express-handlebars')
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+  res.render('index')
+})
+
+app.get('/restaurants/1', (req, res) => {
+  res.render('show')
+})
+
+app.listen(port, () =>{
+  console.log(`connetcting to http://localhost:3000`)
+})
