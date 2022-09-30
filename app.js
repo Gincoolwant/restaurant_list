ae1c0c30ab1afbec7fbf0013d5a2a671b912897c
Gincoolwant <soulbox790326@gmail.com>
2022-09-30 15:10:00 +0800

    [load]load restaurant List form AC

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const restaurants = require('./restaurant.json')
 
 const exphbs = require('express-handlebars')
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
