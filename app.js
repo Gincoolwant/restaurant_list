ec521cc7f42bc33af0717c4e7e12ab2b55e8e1ee
Gincoolwant <soulbox790326@gmail.com>
2022-10-01 18:36:47 +0800

    [feat] trimming keyword while searching & add cover img

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ const port = 3000
 const restaurantList = require('./restaurant.json')
 
 const exphbs = require('express-handlebars')
-app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
-app.set('view engine', 'handlebars')
+app.engine('handlebars', exphbs({ defaultLayout: 'main' })) // 定義模板引擎, (ext:.handlebars, 設定初始layout: main.handlebars)
+app.set('view engine', 'handlebars') // express設置註冊模板引擎
 
 app.use(express.static('public'))
 
@@ -15,16 +15,17 @@ app.get('/', (req, res) => {
 
 app.get('/restaurants/:restaurant_id', (req, res) => {
   const restaurant = restaurantList.results.find(restaurant => restaurant.id.toString() === req.params.restaurant_id)
-  res.render('show', { restaurant: restaurant})
+  res.render('show', { restaurant })
 })
 
 app.get('/search', (req, res) => {
-  const keyword = req.query.keyword
+  const keyword = req.query.keyword.trim()
   const restaurant = restaurantList.results.filter(restaurant => (restaurant.name.toLowerCase().includes(keyword.toLowerCase())) || restaurant.category.includes(keyword))
+
   if(restaurant.length){
-    res.render('index', { restaurant: restaurant, keyword: keyword})
+    res.render('index', { restaurant, keyword })
   }else{
-    res.render('noMatchCase', {keyword: keyword })
+    res.render('noMatchCase', {keyword })
   }
   
 })
