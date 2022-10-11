9f9f57f1fb95a64bf92833d83acb65773d2ba002
Gincoolwant <soulbox790326@gmail.com>
2022-10-11 15:38:55 +0800

    [feat] modify index using MongoDB data

--- app copy.js
+++ app copy.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const port = 3000
-const Restaurant = require('./models/restaurant')
+const restaurantList = require('./restaurant.json').results
 
 // 使用Mongoose與MongoDB連線
 // 載入 mongoose
@@ -16,34 +16,30 @@ db.once('open', () => console.log('mongodb is connecting'))
 
 // 使用template engine
 const exphbs = require('express-handlebars')
-const restaurant = require('./models/restaurant')
 app.engine('handlebars', exphbs({ defaultLayout: 'main' })) // 定義模板引擎, (ext:.handlebars, 設定初始layout: main.handlebars)
 app.set('view engine', 'handlebars') // express設置註冊模板引擎
 app.use(express.static('public')) // 告訴express每次先讀取靜態檔的位置
 
 app.get('/', (req, res) => {
-  Restaurant.find()
-    .lean()
-    .then(restaurant => res.render('index', { restaurant }))
-    .catch(error => console.error(error))
+  res.render('index', { restaurant: restaurantList})
 })
 
-// app.get('/restaurants/:restaurant_id', (req, res) => {
-//   const restaurant = restaurantList.find(restaurant => restaurant.id.toString() === req.params.restaurant_id)
-//   res.render('show', { restaurant })
-// })
+app.get('/restaurants/:restaurant_id', (req, res) => {
+  const restaurant = restaurantList.find(restaurant => restaurant.id.toString() === req.params.restaurant_id)
+  res.render('show', { restaurant })
+})
 
-// app.get('/search', (req, res) => {
-//   const keyword = req.query.keyword.trim()
-//   const restaurant = restaurantList.filter(restaurant => restaurant.name.toLowerCase().includes(keyword.toLowerCase()) || restaurant.category.includes(keyword))
+app.get('/search', (req, res) => {
+  const keyword = req.query.keyword.trim()
+  const restaurant = restaurantList.filter(restaurant => restaurant.name.toLowerCase().includes(keyword.toLowerCase()) || restaurant.category.includes(keyword))
 
-//   if(restaurant.length){
-//     res.render('index', { restaurant, keyword })
-//   }else{
-//     res.render('noMatchCase', {keyword })
-//   }
+  if(restaurant.length){
+    res.render('index', { restaurant, keyword })
+  }else{
+    res.render('noMatchCase', {keyword })
+  }
   
-// })
+})
 
 app.listen(port, () =>{
   console.log(`connecting to http://localhost:3000`)
