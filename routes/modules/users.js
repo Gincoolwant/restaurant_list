9c9045eb7d71d0f9568a5e8eeb4623107f75fba3
Gincoolwant <soulbox790326@gmail.com>
2022-11-04 14:25:06 +0800

    feat: user register feature

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,11 +1,29 @@
 const express = require('express')
 const router = express.Router()
+const User = require('../../models/user.js')
 
 router.get('/register', (req, res) => {
   res.render('register')
 })
 
 router.post('/register', (req, res) => {
+  // 取得註冊參數
+  const { name, email, password, confirmPassword } = req.body
+  // 檢查是否已註冊
+  User.findOne({ email })
+    .then(user => {
+      // 已註冊: 返回註冊頁面
+      if (user) return res.render('register', { name, email, password, confirmPassword })
+      // 未註冊: 資料庫新增使用者後重導至登入頁
+      return User.create({
+        name,
+        email,
+        password
+      })
+        .then(() => res.redirect('/users/login'))
+        .catch(err => console.log(err))
+    })
+    .catch(err => console.log(err))
 
 })
 
