fd6ef39c9d363d4341098ad4012134c15969bcf2
Gincoolwant <soulbox790326@gmail.com>
2022-11-04 17:15:18 +0800

    feat: user authenticator feature in authenticated routes

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const router = express.Router()
+
 const home = require('./modules/home')
 const restaurants = require('./modules/restaurants')
 const users = require('./modules/users')
+const { authenticator } = require('../middleware/auth.js')
 
 router.use('/users', users)
-router.use('/restaurants/', restaurants)
-router.use('/', home)
+router.use('/restaurants/', authenticator,restaurants)
+router.use('/', authenticator, home)
 
 module.exports = router
